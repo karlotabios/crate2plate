34504d36e2f6fc15240d107b1287e1f9411051a7
Karlo Tabios <karlotabios@yahoo.com>
2017-04-22 15:41:14 +0800

    Re-arranged the git para nasa root folder yung gemfile para makita ni heroku agad

--- create_models.bat
+++ create_models.bat
@@ -6,6 +6,6 @@ rails g model Product product_name:string category:string price:decimal
 rails g model Account first_name:string last_name:string phone_number:int billing_unit_number:int billing_street:string billing_village:string billing_building:string billing_landmark:string billing_city:string delivery_unit_number:int delivery_street:string delivery_village:string delivery_building:string delivery_landmark:string delivery_city:string outstanding_balance:decimal account_type:string
 rails g model Payment_History amount_of_payment:decimal date_settled:datetime
 rails g model Supply quantity:string date_of_supply:datetime
-rails g model Admin admin_email:string admin_email:string admin_email:password
-rails g model Customer customer_email:string customer_email:string customer_email:password
+rails g model Admin admin_email:string admin_username:string admin_password:string
+rails g model Customer customer_email:string customer_username:string customer_password:string
 rails g model Business business_email:string business_name:string
