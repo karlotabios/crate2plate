11b9f546dbe36a284e774c90c97e9ebdffcb7960
Karlo Tabios <karlotabios@yahoo.com>
2017-05-03 01:35:38 +0800

    Karlo 5/3/2017 1:34AM: Added placeholder partials for customer side navbar and footer. Added routes temporarily to admin side (using namespace). Added a bit of logic to the controller handling the index (alluding to showing all products on homepage)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root(to: "pages#index")
+  root(to: "customers#index")
+
+  # namespace :admin do
+  # 	# basic CRUD for admin side. Will revise because it's not that simple sa flow ng website mismo
+  # 	resources :supplies
+  # 	resources :orders
+  # end
 end
