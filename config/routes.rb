9457048bb54873442a55da16376331c5a922d709
Karlo Tabios <karlotabios@yahoo.com>
2017-05-03 22:53:23 +0800

    Karlo 5/3/2017 10:52PM: Remade Devise Controllers to a custom User controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-	get "/sign_up" => "devise/registrations#new", as: "new_user_registration"
-  devise_for :users
+	#get "/sign_up" => "devise/registrations#new", as: "new_user_registration"
+  devise_for :users, controllers: { sessions: "users/sessions", confirmations: "users/confirmations", registrations: "users/registrations", passwords: "users/passwords", unlocks: "users/unlocks",  }
   root(to: "customers#index")
   resources :orders
 
